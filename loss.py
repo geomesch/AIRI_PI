b4ec6c9dd50569139a86f88db2c2c4f26764cf43
georgy.m <iam@georgy.top>
2023-07-25 17:55:16 +0300

    Add CMD interface

--- loss.py
+++ loss.py
@@ -9,8 +9,12 @@ class MaskedMSELoss(nn.MSELoss):
             n = x.shape[0]
             loss = 0.0
             diff = (x - y) ** 2
+            b = mask.dtype == torch.bool
             for diff, mask in zip(diff, mask):
-                loss += torch.masked_select(diff, mask).mean()
+                if b:
+                    loss += torch.masked_select(diff, mask).mean()
+                else:
+                    loss += (diff * mask).mean()
             loss /= n
         else:
             shape = tuple(range(1, len(x.shape)))
@@ -19,4 +23,27 @@ class MaskedMSELoss(nn.MSELoss):
             
             
         
-        
+class PILoss(nn.Module):
+    def __init__(self, spacing=(25, 50), s=2e-4):
+        super().__init__()
+        self.spacing = spacing
+        self.s_ = s
+        self.mse = MaskedMSELoss()
+    
+    def forward(self, p, K, mask=None):
+        divk = self.calc_divk(p, K)
+        p_dt = self.calc_p_dt(p)
+        diff = divk - p_dt * self.s_
+        q = torch.zeros_like(diff)
+        q[..., 32, 32] = 1.0
+        diff -= q
+        return self.mse(diff ** 2, 0.0, mask=mask)
+    
+    def calc_divk(self, p, K):
+        p_dx, p_dy = torch.gradient(p, dim=(-2, -1), edge_order=2, spacing=self.spacing)
+        divk = torch.gradient(K * p_dx, dim=-2, edge_order=2, spacing=self.spacing[0])[0]\
+            + torch.gradient(K * p_dy, dim=-1, edge_order=2, spacing=self.spacing[1])[0]
+        return divk
+    
+    def calc_p_dt(self, p):
+        return torch.gradient(p, dim=1, edge_order=2)[0]
--- utils.py
+++ utils.py
@@ -3,6 +3,18 @@ import numpy as np
 import random
 
 def set_seed(seed):
+    torch.use_deterministic_algorithms(True)
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+
+def calc_mask(K, dilation=0, eps=1e-2):
+    mask = K != 0
+    if dilation:
+        with torch.no_grad():
+            if dilation == 'precond':
+                ind = tuple(1 for _ in range(len(K.shape) - 1))
+                mask = K / K.flatten(1).max(1)[0].reshape(-1, *ind) + eps
+            else:
+                mask = torch.nn.functional.max_pool2d(K, dilation, padding=dilation // 2, stride=1).bool()
+    return mask
